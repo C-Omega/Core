c9c80d8d78d53d6c7ab79c83e39613aebd05ac56
Cyclic3 <harlan.connor@gmail.com>
2016-11-14 12:35:02 +0000

    Colour logging

--- Logger.fs
+++ Logger.fs
@@ -3,4 +3,15 @@ module Logger =
     type Logger = 
         {log : string -> unit}
         static member Printn = {log = System.Console.WriteLine}
+        static member CPrintn background foreground = 
+            {log = 
+                (fun s -> 
+                    let b, f = System.Console.BackgroundColor, System.Console.ForegroundColor
+                    System.Console.BackgroundColor <- background
+                    System.Console.ForegroundColor <- foreground
+                    System.Console.WriteLine s
+                    System.Console.BackgroundColor <- b
+                    System.Console.ForegroundColor <- f
+                )
+            }
     let logf (log:Logger) pfs = Printf.kprintf log.log pfs
